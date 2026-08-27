97ec524df109be290f25230874bdfbafebdea5a3
zubair-lol-007 <83009954+zubair-lol-007@users.noreply.github.com>
2026-08-27 23:39:51 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+// Service Worker for ZUBI Downloader
+
+const CACHE_NAME = 'zubi-v1';
+const urlsToCache = [
+  '/zubi-web/',
+  '/zubi-web/index.html',
+  'https://zubair-lol-007.github.io/zubi-web/'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache opened');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate and clean old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
